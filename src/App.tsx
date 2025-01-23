443d52eff292a93216cd3169819cf8269c445b64
Dilith Achalan <dilith.achalan@outlook.com>
2025-01-23 23:28:31 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Contact from './components/Contact';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/hw">
       <Routes>
         <Route path="/contact" element={<Contact />} />
         <Route path="/" element={<Navigate to="/contact" replace />} />
