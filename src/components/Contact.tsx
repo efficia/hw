1bbff02b5a4168e3b345ef88ceaba0e31d26332f
Dilith Achalan <dilith.achalan@outlook.com>
2025-01-27 16:37:34 +0530

    first commit

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -103,7 +103,7 @@ END:VCARD`;
                       className="flex items-center justify-center w-full px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
                     >
                       <Download className="w-5 h-5 mr-2" />
-                      Save Contact
+                      Download Contact
                     </a>
                   </div>
                 </div>
